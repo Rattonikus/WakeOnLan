f02672acbfedbe7211a156fa4630c2dd0f8e1373
Rattonikus <71366308+Rattonikus@users.noreply.github.com>
2024-02-19 22:27:26 -0700

    initial commit

---
+++ WakeOnLan/Model/WakeOnLanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeOnLanApp.swift
+//  WakeOnLan
+//
+//  Created by Cameron Elkins on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WakeOnLanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WakeOnLan/ViewController/MainView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WakeOnLan
+//
+//  Created by Cameron Elkins on 2/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View 
+{
+    var body: some View 
+    {
+        VStack 
+        {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
